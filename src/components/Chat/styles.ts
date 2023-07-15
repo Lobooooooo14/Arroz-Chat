86b0a7dba39900c4ca184e631e734981735e5eb0
Lobooooooo14 <88998991+Lobooooooo14@users.noreply.github.com>
2023-07-15 12:52:39 -0300

    fix: test fix layout in mobile

--- src/components/Chat/styles.ts
+++ src/components/Chat/styles.ts
@@ -6,10 +6,8 @@ export const Content = styled.div`
     max-width: 400px;
     height: 100vh;
     display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 50px 1fr 50px;
     grid-template-areas: 
-        "header"
-        "messages"
-        "footer";
+        "header" 50px
+        "messages" 1fr
+        "footer" 50px;
 `
