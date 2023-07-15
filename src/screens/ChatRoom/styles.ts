e91357784f22aa33ba795ede8a3e61c593ec96e9
Lobooooooo14 <88998991+Lobooooooo14@users.noreply.github.com>
2023-07-15 13:08:55 -0300

    fix: test use %

--- src/screens/ChatRoom/styles.ts
+++ src/screens/ChatRoom/styles.ts
@@ -4,7 +4,7 @@ import { shade } from "polished"
 
 export const Content = styled.div`
     width: 100%;
-    height: 100vh;
+    height: 100%;
     display: flex;
     justify-content: center;
     background-color: ${props => shade(0.2, props.theme.colors.background)};
