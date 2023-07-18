4755eee9ef0b655334cf3029be367054dc7afa6d
Lobooooooo14 <88998991+Lobooooooo14@users.noreply.github.com>
2023-07-17 22:21:49 -0300

    feat: added gradient in chat messages

--- src/components/Chat/Messages/styles.ts
+++ src/components/Chat/Messages/styles.ts
@@ -6,7 +6,11 @@ export const Content = styled.div`
     grid-area: messages;
     width: 100%;
     height: 100%;
-    background-color: ${props => shade(0.8, props.theme.colors.primary)};
     padding: 0 10px;
     overflow-y: auto;
+    background-image: linear-gradient(
+        45deg,
+        ${props => shade(0.8, props.theme.colors.primary)},
+        ${props => shade(0.4, props.theme.colors.primary)}
+    );
 `
